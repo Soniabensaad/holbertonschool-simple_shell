6e8da5849a897ce2e6431dfe4d081f072d89e1ba
nadaMAoui <maaouinada747@gmail.com>
2022-12-07 01:04:57 +0100

    main functions

--- main.c
+++ main.c
@@ -1,23 +1,48 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int __attribute__ ((unused)) argc, __attribute__ ((unused)) char **argv)
+int main(int __attribute__((unused)) argc, char **argv)
 {
 	char *prompt = "$ ";
 	char *lineptr;
 	size_t n = 0;
-	ssize_t nchars_read;
-
-	while(1)
-	{
-	printf("%s", prompt);
-	nchars_read = getline(&lineptr, &n, stdin);
-	if(nchars_read == 0)
-	return(0);
-	printf("%s\n", lineptr);
+	ssize_t cmd;
+	char *lineptr_copy = NULL, token;
+	const char *delim = " \n";
+	int i, num_token = 0;
 	
-	free(lineptr);
+	while (1)
+	{
+		printf("%s", prompt);
+		cmd = getline(&lineptr, &n, stdin);
+		if (cmd == -1)
+			return (-1);
+		lineptr_copy = malloc(sizeof(char) * cmd);
+		if (lineptr_copy == NULL)
+		{
+			perror("Memory aloocation failed");
+			return (-1);
+		}
+		strcpy(lineptr_copy, lineptr);
+		token = strtok(lineptr, delim);
+		while (token != NULL)
+		{
+			num_token++;
+			token = strtok(NULL, delim);
+		}
+		num_token++;
+		argv = malloc(sizeof(char *) * num_token);
+		token = strtok(lineptr_copy, delim);
+
+		for (i = 0; token != NULL; i++)
+		{
+			argv[i] = malloc(sizeof(char) * strlen(token));
+			strcpy(argv[i], token);
+			token = strtok(NULL, delim);
+		}
+		argv[i] = NULL;
+		execmd(argv);
 	}
+	free(lineptr_copy);
+	free(lineptr);
+
 	return (0);
 }
